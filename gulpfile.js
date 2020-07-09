a1d821e9977cb0037fb10d4608de4942ae24a470
Kate Peleshko <kate200ka@gmail.com>
2020-07-10 01:27:32 +0300

    NEW: Create new test project

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+function style() {
+    return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./scss/**/*.scss', style).on('change', browserSync.reload);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+exports.style = style;
+exports.watch = watch;
+
+gulp.task('start', gulp.parallel(style, watch));
